198a360973da5669bdb7fa6aa4bac9a73571e600
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-01-25 21:50:55 +0000

    fix(tests): fix processor start timestamp

--- tests/processors/test_base.py
+++ tests/processors/test_base.py
@@ -1,13 +1,15 @@
 import pytest
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from generic_exporters.processors._base import _TimeSeriesProcessorBase
 
 from fixtures import *
 
+now = datetime.now()
+
 class DummyTimeSeriesProcessor(_TimeSeriesProcessorBase):
     interval = timedelta(minutes=1)
     async def start_timestamp(self):
-        return datetime.now() - timedelta(days=1)
+        return now - timedelta(days=1)
     async def run(self):
         return
 
@@ -19,4 +21,4 @@ async def test_time_series_processor_base(time_series):
     # Test _timestamps generator
     start_timestamp = await processor.start_timestamp()
     timestamps = [ts async for ts in processor._timestamps()]
-    assert timestamps[0] == start_timestamp
+    assert timestamps[0] == start_timestamp.astimezone(timezone.utc)
